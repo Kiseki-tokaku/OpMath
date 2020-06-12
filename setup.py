dc037592dbc2e6ff34ef597b8bec43fd808edf84
Kiseki-tokaku <ines1703@gmail.com>
2020-06-12 17:18:47 +0100

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='OpMath',
 	version='1.0',
 	description='OpMath Distribution Package',
-	author = 'ines'
+	author = 'ines',
 	author_email='Your Email',
 	url = 'https://github.com/Kiseki-tokaku/OpMath.git',
 	packages = ['OpMath'],
