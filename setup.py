eb5e199c6e9de02dc83e238c32a7ee0de1374a57
Kiseki-tokaku <ines1703@gmail.com>
2020-06-12 17:15:36 +0100

    First commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='OpMath',
 	version='1.0',
 	description='OpMath Distribution Package',
 	author = 'ines'
-	auther_email='Your Email',
+	author_email='Your Email',
 	url = 'https://github.com/Kiseki-tokaku/OpMath.git',
 	packages = ['OpMath'],
 	)
